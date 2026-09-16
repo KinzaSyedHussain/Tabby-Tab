92dae3abc28f81a197e81a103819bb050d573428
Kinza Syed Hussain <kinzasyed060@gmail.com>
2026-09-16 18:42:48 +0300

    enhance the shortcuts and add delete and add button

--- src/main.js
+++ src/main.js
@@ -26,12 +26,12 @@ if (savedBg) {
 }
 
 if (bgColor) {
-  bgColor.addEventListener('change', (e) => {
+  bgColor.addEventListener('input', (e) => {
     const color = e.target.value;
     document.body.style.backgroundImage = 'none';
     document.body.style.backgroundColor = color;
     localStorage.setItem('customBgColor', color);
-    localStorage.removeItem('customBgImage');
+    localStorage.removeItem('customBg');
   });
 }
 
@@ -49,3 +49,86 @@ if (bgUpload) {
   }
 });
 }
+
+const defaultShortcuts = [
+  { title: 'GitHub', url: 'https://github.com'},
+  { title: 'Youtube', url: 'https://youtube.com'},
+  { title: 'Hack Club', url: 'https://hackclub.com' },
+  { title: 'Gmail', url: 'https://gmail.com'}
+]
+
+let shortcuts = JSON.parse(localStorage.getItem('tabbyShortcuts')) || defaultShortcuts;
+
+function getFaviconUrl(url) {
+  try {
+    const domain = new URL(url).hostname;
+    return `https://www.google.com/s2/favicons?domain=${domain}&sz=64`;
+  } catch {
+    return '';
+  }
+}
+
+function renderShortcuts() {
+  const container = document.getElementById('shortcutlist');
+  if (!container) return;
+  container.innerHTML = '';
+
+
+shortcuts.forEach((shortcut, index) => {
+  const link = document.createElement('a');
+  link.href = shortcuts.url;
+  link.className = 'shortcut';
+  link.title = shortcut.title;
+
+  const img = document.createElement('img');
+  img.src = getFaviconUrl(shortcut.url);
+  img.alt = shortcut.title;
+
+  const delBtn = document.createElement('button');
+  delBtn.className = 'delete-btn';
+  delBtn.textContent = 'x';
+  delBtn.onclick = (e) => {
+    e.preventDefault();
+    shortcuts.splice(index, 1);
+    saveShortcuts();
+  };
+
+  link.appendChild(img);
+  link.appendChild(delBtn);
+  container.appendChild(link)
+});
+
+}
+
+function saveShortcuts() {
+  localStorage.setItem('tabbyShortcuts', JSON.stringify(shortcuts));
+  renderShortcuts();
+}
+
+const modal = document.getElementById('shortcut-modal');
+const addBtn = document.getElementById('addshortcut-btn');
+const closeBtn = document.getElementById('close-modal-btn');
+const form = document.getElementById('shortcut-form');
+
+addBtn.addEventListener('click', () => modal.showModal());
+closeBtn.addEventListener('click', () => modal.close());
+
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.getElementById('shortcut-title').value.trim();
+  let url = document.getElementById('shortcut-url').value.trim();
+
+  if (!url.startsWith('http://') && !url.startsWith('https://')) {
+    url = 'https://' + url;
+  }
+
+  if (title && url) {
+    shortcuts.push({ title, url });
+    saveShortcuts();
+    form.reset();
+    modal.close();
+  }
+});
+
+renderShortcuts();
